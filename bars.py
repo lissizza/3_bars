e90655df470f90ba2cb76e684d410b5294f2ff9c
lissizza <lissizza@gmail.com>
2017-01-17 12:33:01 +0000

    adjustments

--- bars.py
+++ bars.py
@@ -5,6 +5,7 @@ from math import sqrt
 def get_distance(user_latitude, user_longitude, bar_latitude, bar_longitude):
     return sqrt((user_latitude - float(bar_latitude))**2 + (user_longitude - float(bar_longitude))**2)
 
+
 def get_coordinates():
     coordinates = input("Введите вашу долготу и широту через пробел: ")
     try:
@@ -12,25 +13,26 @@ def get_coordinates():
         return (longitude, latitude)
     except Exception as e:
         return None
+
     
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath) as data_file:    
         return json.load(data_file)
+
     
 def get_biggest_bar(data):
-    biggest_bar = max(data, key=lambda e: e['SeatsCount'])
-    return (biggest_bar['Name'], biggest_bar['SeatsCount'])
+    return max(data, key=lambda e: e['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    smallest_bar = min(data, key=lambda e: e['SeatsCount'])
-    return (smallest_bar['Name'], smallest_bar['SeatsCount'])
+    return min(data, key=lambda e: e['SeatsCount'])
+
 
 def get_closest_bar(data, longitude, latitude):
-    closest_bar = min(data, key=lambda e: get_distance(latitude, longitude, e['geoData']['coordinates'][1], e['geoData']['coordinates'][0]))
-    return (closest_bar['Name'], str(closest_bar['geoData']['coordinates']))
+    return min(data, key=lambda e: get_distance(latitude, longitude, e['geoData']['coordinates'][1], e['geoData']['coordinates'][0]))
+
 
 if __name__ == '__main__':
     filepath = input('Укажите путь к файлу с данными: ')
@@ -39,12 +41,16 @@ if __name__ == '__main__':
         print('Кажется, с вашим файлом что-то не так, попробуйте еще раз')
         
     else:
-        print('Самый вместительный московский бар: %s, %s посадочных мест' % get_biggest_bar(bar_data))
-        print('Самый тесный московский бар: %s, %s посадочных мест' % get_smallest_bar(bar_data))
+        biggest_bar = get_biggest_bar(bar_data)
+        print('Самый вместительный московский бар: {}, {} посадочных мест'.format(biggest_bar['Name'], biggest_bar['SeatsCount']))
+        
+        smallest_bar = get_smallest_bar(bar_data)
+        print('Самый тесный московский бар: {}, {} посадочных мест'.format(smallest_bar['Name'], smallest_bar['SeatsCount']))
         
         coordinates = get_coordinates()
         if coordinates is None:
             print('Неверно указана широта или долгота, попробуйте еще раз')
         else:
-            print('Ваши координаты: [%s, %s]' % coordinates)
-            print('Ближайший от вас московский бар: %s, его координаты: %s' % get_closest_bar(bar_data, coordinates[0], coordinates[1]))
+            closest_bar = get_closest_bar(bar_data, coordinates[0], coordinates[1])
+            print('Ваши координаты: [{}, {}]'.format(coordinates[0], coordinates[1]))
+            print('Ближайший от вас московский бар: {}, его координаты: {}'.format(closest_bar['Name'], str(closest_bar['geoData']['coordinates'])))
